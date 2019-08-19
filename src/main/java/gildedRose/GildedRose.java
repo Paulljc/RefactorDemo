dcbbc6dca57d68c73b5ded7e0cd207943892297a
Paulljc <714978398@qq.com>
2019-08-19 21:08:31 +0800

    refactor updateItem method to Class Item

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -9,56 +9,8 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            updateItem(items[i]);
+            items[i].updateItem();
         }
     }
 
-    private void updateItem(Item item) {
-        String itemName = item.name;
-        switch (itemName) {
-            case "Aged Brie":
-                if (item.quality < 50) {
-                    item.quality++;
-                }
-                item.sellIn--;
-                if (item.sellIn < 0) {
-                    if (item.quality < 50) {
-                        item.quality++;
-                    }
-                }
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                if (item.quality < 50) {
-                    item.quality++;
-                }
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality++;
-                    }
-                }
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-                item.sellIn = item.sellIn - 1;
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-                break;
-            default:
-                if (item.quality > 0) {
-                    item.quality--;
-                }
-                item.sellIn = item.sellIn - 1;
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-                break;
-        }
-    }
 }
--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -8,14 +8,65 @@ public class Item {
 
     public int quality;
 
+
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
+
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    void updateItem() {
+        String itemName = name;
+        switch (itemName) {
+            case "Aged Brie":
+                if (quality < 50) {
+                    quality++;
+                }
+                sellIn--;
+                if (sellIn < 0) {
+                    if (quality < 50) {
+                        quality++;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (quality < 50) {
+                    quality++;
+                }
+                if (sellIn < 11) {
+                    if (quality < 50) {
+                        quality++;
+                    }
+                }
+                if (sellIn < 6) {
+                    if (quality < 50) {
+                        quality = quality + 1;
+                    }
+                }
+                sellIn = sellIn - 1;
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+                if (quality > 0) {
+                    quality--;
+                }
+                sellIn = sellIn - 1;
+                if (sellIn < 0) {
+                    if (quality > 0) {
+                        quality = quality - 1;
+                    }
+                }
+                break;
+        }
     }
 }
