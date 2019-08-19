f8c9bc4e180c103c9759a1d036c67d8aef95a2f6
Paulljc <714978398@qq.com>
2019-08-19 19:35:29 +0800

    finish fourth situation in Backstage test

--- src/test/java/gildedRose/GlidedRoseTest.java
+++ src/test/java/gildedRose/GlidedRoseTest.java
@@ -96,4 +96,15 @@ public class GlidedRoseTest {
         System.out.println(gildedRose.items[0]);
         Assert.assertEquals("Backstage passes to a TAFKAL80ETC concert, 3, 50", gildedRose.items[0].toString());
     }
+
+    @Test
+    public void should_return_Backstage_minus1_0_when_invoke_updateQuality_Backstage_0_50() {
+        Item[] items = new Item[] {new Item(BACKSTAGE, 0, 50)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        System.out.println(gildedRose.items[0]);
+        Assert.assertEquals("Backstage passes to a TAFKAL80ETC concert, -1, 0", gildedRose.items[0].toString());
+    }
 }
