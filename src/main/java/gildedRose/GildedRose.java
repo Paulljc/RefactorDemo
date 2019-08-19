3030f47e13a9d9efd02810e07953359fe0adeded
Paulljc <714978398@qq.com>
2019-08-19 20:58:06 +0800

    use switch to implement Backstage

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -28,6 +28,23 @@ public class GildedRose {
                 }
                 break;
             case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.quality < 50) {
+                    item.quality++;
+                }
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality++;
+                    }
+                }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
                 break;
             case "Sulfuras, Hand of Ragnaros":
                 break;
