48ae59dcf54e8dfa574914761d2f552d16eb5f50
Paulljc <714978398@qq.com>
2019-08-19 21:01:06 +0800

    use switch to implement Sulfuras and default situation

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -49,6 +49,15 @@ public class GildedRose {
             case "Sulfuras, Hand of Ragnaros":
                 break;
             default:
+                if (item.quality > 0) {
+                    item.quality--;
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
                 break;
         }
     }
